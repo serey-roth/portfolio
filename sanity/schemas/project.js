9596fd6493beb042bd82c37692236f8761a54653
Serey Roth <sereyratanakroth@gmail.com>
2022-12-26 18:52:28 -0800

    create project schema

---
+++ sanity/schemas/project.js
@@ -0,0 +1,59 @@
+export default {
+    name: 'project',
+    title: 'Project',
+    type: 'document',
+    fields: [
+        {
+            name: 'name', 
+            title: 'Name',
+            type: 'string',
+        },
+        {
+            name: 'description', 
+            title: 'Description',
+            type: 'string',
+        },
+        {
+            name: 'startDate', 
+            title: 'Start Date',
+            type: 'date',
+        },
+        {
+            name: 'endDate', 
+            title: 'End Date',
+            type: 'date',
+        },
+        {
+            name: 'tools',
+            title: 'Tools',
+            type: 'array',
+            of: [{
+                type: 'reference',
+                to: [{
+                    type: 'tool'
+                }]
+            }]
+        },
+        {
+            name: 'images', 
+            title: 'Images',
+            type: 'array', 
+            of: [{
+                type: 'image',
+                options: {
+                    hotspot: true,
+                }
+            }]
+        }, 
+        {
+            name: 'link_demo',
+            title: 'Demo',
+            type: 'string'
+        },
+        {
+            name: 'link_source',
+            title: 'Source',
+            type: 'string'
+        }
+    ]
+}
