68cd3e8b66cd5b3fdb75cdbb8677c49ed7aadef1
Serey Roth <sereyratanakroth@gmail.com>
2022-12-26 18:52:42 -0800

    create tool schema

---
+++ sanity/schemas/tool.js
@@ -0,0 +1,20 @@
+export default {
+    name: 'tool', 
+    title: 'Tool', 
+    type: 'document',
+    fields: [
+        {
+            name: 'name', 
+            title: 'Name',
+            type: 'string',
+        },
+        {
+            name: 'icon', 
+            title: 'Icon',
+            type: 'image',
+            options: {
+                hotspot: true,
+            }
+        }
+    ]
+}
