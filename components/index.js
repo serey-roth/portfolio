72a0d5026935a45ae245074d28bef28c58c921fb
Serey Roth <sereyratanakroth@gmail.com>
2022-12-28 15:01:26 -0800

    export all components from components/index.js

---
+++ components/index.js
@@ -0,0 +1,7 @@
+export { default as About }  from '../components/About';
+export { default as Footer }  from '../components/Footer';
+export { default as Header }  from '../components/Header';
+export { default as Navbar }  from '../components/Navbar';
+export { default as NavigationDots }  from '../components/NavigationDots';
+export { default as Projects }  from '../components/Projects';
+export { default as Technologies }  from '../components/Technologies';
