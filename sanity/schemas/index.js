139a8741bcb168ecf42f81cd510783899380bacb
Serey Roth <sereyratanakroth@gmail.com>
2022-12-27 21:55:33 -0800

    add message and about schemas to Sanity

--- sanity/schemas/index.js
+++ sanity/schemas/index.js
@@ -1,4 +1,6 @@
 import project from "./project"
 import tool from "./tool"
+import message from "./message";
+import about from "./about";
 
-export const schemaTypes = [project, tool]
+export const schemaTypes = [project, tool, message, about];
