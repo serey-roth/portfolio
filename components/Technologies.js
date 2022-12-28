d51c607214ea3e8a286e799f24b70aba4d98e9ed
Serey Roth <sereyratanakroth@gmail.com>
2022-12-27 19:28:06 -0800

    create technologies section

--- components/Technologies.js
+++ components/Technologies.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import Marquee from './Marquee'
+import LoadingImage from './LoadingImage'
 
-const Technologies = () => {
+import { urlFor } from '../lib/client'
+
+const Technologies = ({ technologies }) => {
     return (
-        <div>Technologies</div>
+        <div 
+        id='technologies'
+        className='w-screen flex flex-col items-center pt-20'>
+            <h1 className='w-full font-bold text-3xl lg:text-5xl py-2 mb-5 px-10 xl:w-[80%]'>Technologies</h1>
+            <Marquee>
+                <div className='w-full flex gap-5 items-center py-5'>
+                    {technologies.map(technology => (
+                        <LoadingImage 
+                                key={technology.name}
+                                src={urlFor(technology.icon).url()}
+                                alt={technology.name}
+                                width={'150px'}
+                                height={'150px'}
+                                classes='drop-shadow-xl'
+                            />
+                    ))}
+                </div>
+            </Marquee>
+        </div>
     )
 }
 
