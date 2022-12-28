c267f0d2449d4dc2583e151742ac0c7154831c01
Serey Roth <sereyratanakroth@gmail.com>
2022-12-27 21:55:41 -0800

    create message schema

---
+++ sanity/schemas/message.js
@@ -0,0 +1,22 @@
+export default {
+    name: 'message',
+    title: 'Message',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+        },
+        {
+            name: 'email',
+            title: 'Email',
+            type: 'string',
+        },
+        {
+            name: 'message',
+            title: 'Message',
+            type: 'string',
+        }
+    ]
+}
