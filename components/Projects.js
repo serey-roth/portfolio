ed0b610b58ae693bd2e8d2273bd4a9f6b9ccd9c6
Serey Roth <sereyratanakroth@gmail.com>
2022-12-27 19:27:53 -0800

    increase gap size for Projects component

--- components/Projects.js
+++ components/Projects.js
@@ -7,8 +7,8 @@ const Projects = ({ projects }) => {
         id='projects' 
         className='w-screen min-h-screen pt-20 px-10 xl:w-[80%]'>
             <h1 className='font-bold text-3xl lg:text-5xl py-2 mb-2'>Projects</h1>
-            <div className='flex flex-col md:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 
-            2xl:grid-cols-5 place-items-stretch gap-3'>
+            <div className='flex flex-col md:grid grid-cols-2 lg:grid-cols-3 
+            2xl:grid-cols-5 place-items-stretch gap-5'>
                 {projects.map(project => (
                     <Project key={project._id} project={project} />
                 ))}
