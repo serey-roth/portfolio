101569aa6d23d0994a060b19e217628b8b867c22
Serey Roth <sereyratanakroth@gmail.com>
2022-12-29 00:05:40 -0800

    update styles in NavigationDots

--- components/NavigationDots.js
+++ components/NavigationDots.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import Link from 'next/link';
-import { RxDotFilled } from 'react-icons/rx';;
+import { RxDotFilled } from 'react-icons/rx';
+import { SECTIONS } from '../lib';
 
 const NavigationDots = ({ currentSectionIndex, onClick}) => {
     return (
         <div className='flex flex-col w-fit h-fit'>
-            {['home', 'about', 'projects', 'technologies', 'contact'].map((item, index) => (
+            {SECTIONS.map((item, index) => (
                 <Link 
                 key={`${item}-dot`}
                 onClick={() => onClick(index)}
                 href={`#${item}`}>
                     <RxDotFilled 
                     size={25} 
-                    className={`duration-200 ease-in-out text-indigo-500
-                    ${index === currentSectionIndex ? 'opacity-100' : 'opacity-50'}`}/>
+                    className={`duration-200 ease-in-out text-indigo-500 scale-100
+                    ${index === currentSectionIndex ? 'opacity-100 scale-150' : 'opacity-30'}`}/>
                 </Link>
             ))}
         </div>
