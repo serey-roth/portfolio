47af2cb771dd83da397b97cc62d03784259c7670
Serey Roth <sereyratanakroth@gmail.com>
2022-12-29 00:04:56 -0800

    replace Footer with Contact in component/index.js

--- components/index.js
+++ components/index.js
@@ -1,5 +1,5 @@
 export { default as About }  from '../components/About';
-export { default as Footer }  from '../components/Footer';
+export { default as Contact }  from '../components/Contact';
 export { default as Hero }  from '../components/Hero';
 export { default as Navbar }  from '../components/Navbar';
 export { default as NavigationDots }  from '../components/NavigationDots';
