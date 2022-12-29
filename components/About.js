5ea6d158569fc6e0be0c2342f44f232d4155391e
Serey Roth <sereyratanakroth@gmail.com>
2022-12-28 18:37:26 -0800

    add animations to About while in view

--- components/About.js
+++ components/About.js
@@ -1,31 +1,64 @@
-import React from 'react'
-import { urlFor } from '../lib/client'
-import LoadingImage from './LoadingImage'
+import React from 'react';
+import { motion } from 'framer-motion';
+import { urlFor } from '../lib';
+
+const imageVariants = {
+    hidden: {
+        opacity: 0,
+    },
+    show: {
+        opacity: 1,
+        transition: {
+            duration: 0.4,
+            ease: 'easeInOut'
+        }
+    }
+}
+
+const aboutVariants = {
+    hidden: {
+        opacity: 0,
+    }, 
+    show: {
+        opacity: 1,
+        transition: {
+            staggerChildren: 500,
+        }
+    }
+}
 
 const About = React.forwardRef(({ abouts }, ref) => {
     return (
         <div 
         ref={ref}
         id='about' 
-        className='w-full pt-20 px-10 xl:w-[80%]'>
-            <h1 className='font-bold text-3xl lg:text-5xl py-2 mb-2'>I am a...</h1>
+        className='w-full flex flex-col pt-20 px-10 xl:w-[80%]'>
+            <h1
+            className='font-bold text-3xl lg:text-5xl py-2 mb-2'>
+                I am a...
+            </h1>
             <div className='flex w-full flex-col md:flex-row md:flex-wrap items-center md:items-start
             justify-center gap-4 pt-10'>
                 {abouts.map((item, index) => (
-                    <div 
+                    <motion.div 
+                    variants={aboutVariants}
+                    initial='hidden'
+                    whileInView='show'
                     key={index}
                     className='flex flex-col items-center max-w-[300px] gap-4 p-2'>
-                        <LoadingImage 
+                        <motion.img 
+                            variants={imageVariants}
+                            initial='hidden'
+                            whileInView='show'
                             src={urlFor(item.icon).url()}
                             alt={item.title}
-                            height={'300px'}
-                            width={'300px'}
-                            classes='max-w-[200px] max-h-[200px] drop-shadow-xl' />
-                        <h3 className='font-semibold text-xl capitalize'>{item.title}</h3>
+                            className='max-w-[100px] max-h-[100px]
+                            md:max-w-[150px] md:max-h-[150px] drop-shadow-xl' />
+                        <h3 className='font-semibold text-xl md:text-2xl capitalize'>{item.title}</h3>
                         <p>
                         {item.description}
                         </p>
-                    </div>
+                    </motion.div>
                 ))}
             </div>
         </div>
