b307f3baaaa4fabdf5c2af68a2cb2fc04037c028
Serey Roth <sereyratanakroth@gmail.com>
2022-12-28 19:55:08 -0800

    replace Header with Hero

--- components/index.js
+++ components/index.js
@@ -1,6 +1,6 @@
 export { default as About }  from '../components/About';
 export { default as Footer }  from '../components/Footer';
-export { default as Header }  from '../components/Header';
+export { default as Hero }  from '../components/Hero';
 export { default as Navbar }  from '../components/Navbar';
 export { default as NavigationDots }  from '../components/NavigationDots';
 export { default as Projects }  from '../components/Projects';
