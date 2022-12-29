8b9819b51c07974e331cf6ccc126b5803ee3d55a
Serey Roth <sereyratanakroth@gmail.com>
2022-12-28 18:37:39 -0800

    update styles of project container

--- components/Projects.js
+++ components/Projects.js
@@ -6,7 +6,7 @@ const Projects = React.forwardRef(({ projects }, ref) => {
         <div 
         ref={ref}
         id='projects' 
-        className='w-screen min-h-screen pt-20 px-10 xl:w-[80%]'>
+        className='w-screen flex flex-col pt-20 px-10 xl:w-[80%]'>
             <h1 className='font-bold text-3xl lg:text-5xl py-2 mb-2'>Projects</h1>
             <div className='flex flex-col md:grid grid-cols-2 lg:grid-cols-3 
             2xl:grid-cols-5 place-items-stretch gap-5'>
