ea4222df6bd836b6f5da5cbbb230ea247e5bf207
Serey Roth <sereyratanakroth@gmail.com>
2022-12-28 18:39:41 -0800

    create hero schema

---
+++ sanity/schemas/hero.js
@@ -0,0 +1,28 @@
+export default {
+    name: 'hero',
+    title: 'Hero',
+    type: 'document',
+    fields: [
+        {
+            name: 'description',
+            title: 'Description',
+            type: 'string',
+        },
+        {
+            name: 'photo',
+            title: 'Photo',
+            type: 'image',
+            options: {
+                hotspot: true,
+            }
+        }, 
+        {
+            name: 'tags',
+            title: 'Tags',
+            type: 'array',
+            of : [{
+                type: 'string',
+            }]
+        }
+    ]
+};
