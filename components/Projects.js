00cb9210ae69a84e70aa04fd2e2295e40ae39555
Serey Roth <sereyratanakroth@gmail.com>
2022-12-29 00:06:09 -0800

    update styles in Projects

--- components/Projects.js
+++ components/Projects.js
@@ -6,8 +6,8 @@ const Projects = React.forwardRef(({ projects }, ref) => {
         <div 
         ref={ref}
         id='projects' 
-        className='w-screen flex flex-col pt-20 px-10 xl:w-[80%]'>
-            <h1 className='font-bold text-3xl lg:text-5xl py-2 mb-2'>Projects</h1>
+        className='section'>
+            <h1 className='section-heading'>Projects</h1>
             <div className='flex flex-col md:grid grid-cols-2 lg:grid-cols-3 
             2xl:grid-cols-5 place-items-stretch gap-5'>
                 {projects.map(project => (
