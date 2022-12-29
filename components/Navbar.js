3de0c36125e54cbb729e0e48805ad450708133ab
Serey Roth <sereyratanakroth@gmail.com>
2022-12-29 00:05:23 -0800

    update styles and animations in Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 import Link from 'next/link';
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion, AnimatePresence } from 'framer-motion';
+import { SECTIONS } from '../lib';
 
 const variants = {
-    initial: { x: 300, opacity: 0 },
+    initial: { x: '100%', opacity: 0 },
     animate: {
         opacity: 1,
-        x: [300, 0],
+        x: ['100%', '0%'],
         transition: {
             x: {
                 duration: 0.85,
@@ -16,7 +17,7 @@ const variants = {
         }
     },
     exit: {
-        x: 300,
+        x: '100%',
         transition: {
             x: {
                 duration: 0.85,
@@ -41,7 +42,7 @@ const Navbar = () => {
             uppercase'>Serey</h1>
             <ul className='h-full items-center justify-center gap-3
             hidden lg:flex'>
-                {['home', 'about', 'projects', 'technologies', 'contact'].map(item => (
+                {SECTIONS.map(item => (
                     <li key={`link-${item}`} className='opacity-80 group
                     group-hover:opacity-100
                     ease-in-out duration-200'>
@@ -49,8 +50,8 @@ const Navbar = () => {
                             href={`#${item}`}
                             className='uppercase px-5 py-3 rounded-l-full
                             rounded-r-full font-semibold text-teal-500/80 duration-500
-                            ease-in-out group-hover:bg-gradient-to-r
-                            from-indigo-500/50 to-teal-500/50 group-hover:text-white'>
+                            ease-in-out group-hover:bg-gradient-to-r from-indigo-500/50
+                            to-teal-500/50 group-hover:text-white xl:text-xl 2xl:text-2xl'>
                             {item}
                         </Link>
                     </li>
@@ -66,35 +67,52 @@ const Navbar = () => {
                 <AnimatePresence>
                 {toggle && (
                     <motion.div
-                        key='side-nav-bar'
-                        variants={variants}
-                        initial='initial'
-                        animate='animate'
-                        exit='exit'
-                        className='fixed top-0 bottom-0 right-0 z-20
-                        w-[80%] h-screen flex flex-col justify-end pl-4
-                        items-end drop-shadow-xl bg-indigo-100'
-                    >
-                        <div
-                            className='cursor-pointer p-5 rounded-full'
-                            onClick={handleToggle}>
-                            <HiX size={30} />
-                        </div>
-                        <ul className='w-full h-full flex flex-col items-start justify-start
-                        gap-4'>
-                        {['home', 'about', 'projects', 'technologies', 'contact'].map(item => (
-                            <li key={item}>
-                                <Link
-                                    className='uppercase text-teal-500/80 duration-200
-                                    ease-in-out hover:text-indigo-500 hover:underline
-                                    underline-offset-8 font-semibold'
-                                    href={`#${item}`}
-                                    onClick={handleToggle}>
-                                    {item}
-                                </Link>
-                            </li>
-                        ))}
-                        </ul>
+                    transition={{
+                        delayChildren: 0.2,
+                        staggerChildren: 0.5
+                    }}>
+                        <motion.div
+                            key='side-nav-bar'
+                            variants={variants}
+                            initial='initial'
+                            animate='animate'
+                            exit='exit'
+                            className='fixed top-0 bottom-0 right-0 z-20
+                            w-[80%] mobile-md:w-[60%] h-screen flex flex-col 
+                            justify-end pl-4
+                            items-end drop-shadow-xl bg-gradient-to-tr from-indigo-200 to-teal-400
+                            shadow-lg shadow-indigo-500'
+                        >
+                            <div
+                                className='cursor-pointer p-5 rounded-full text-teal-800'
+                                onClick={handleToggle}>
+                                <HiX size={30} />
+                            </div>
+                            <ul className='w-full h-full flex flex-col items-start 
+                            justify-start gap-4'>
+                            {SECTIONS.map(item => (
+                                <li key={item}>
+                                    <Link
+                                        className='uppercase text-gray-100 duration-200
+                                        ease-in-out hover:text-gradient hover:underline
+                                        underline-offset-8 font-bold text-2xl md:text-3xl'
+                                        href={`#${item}`}
+                                        onClick={handleToggle}>
+                                        {item}
+                                    </Link>
+                                </li>
+                            ))}
+                            </ul>
+                        </motion.div>
+                        <motion.div
+                            key='side-nav-bar-overlay'
+                            variants={variants}
+                            initial='initial'
+                            animate='animate'
+                            exit='exit'
+                            className='fixed top-0 bottom-0 right-0 z-10
+                            w-full h-screen bg-black/80'
+                        />
                     </motion.div>
                 )}
                 </AnimatePresence>
