5730e740581187ddaf2f35aa6087c446f091cb03
Serey Roth <sereyratanakroth@gmail.com>
2022-12-28 16:34:09 -0800

    update image slider with framer motion div and add slide and drag animations

--- components/ImageSlider.js
+++ components/ImageSlider.js
@@ -1,60 +1,105 @@
-import React, { useState } from 'react'
-import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs'
-import { RxDotFilled } from 'react-icons/rx'
-import { urlFor } from '../lib/client'
-import LoadingImage from './LoadingImage'
+import React, { useState } from 'react';
+import { motion, wrap, AnimatePresence } from 'framer-motion';
+
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import { urlFor } from '../lib';
+
+const variants = {
+    enter: (direction) => {
+        return {
+            x: direction > 0 ? 20 : -20,
+            opacity: 0,
+        };
+    },
+    center: {
+        zIndex: 1,
+        x: 0,
+        opacity: 1,
+    },
+    exit: (direction) => {
+        return {
+            zIndex: 1,
+            x: direction < 0 ? 20 : -20,
+            opacity: 0,
+        };
+    }
+};
+
+const swipeConfidenceThreshold = 10000;
+
+const swipePower = (offset, velocity) => {
+    return Math.abs(offset) * velocity;
+};
+
+const arrowBaseClasses = `cursor-pointer bg-black/80 text-white rounded-full 
+drop-shadow-2xl absolute top-1/2 -translate-y-1/2 z-[3] p-2 
+scale-100 2xl:scale-120`;
 
 const ImageSlider = ({ images }) => {
-    const [currentIndex, setCurrentIndex] = useState(0);
+    const [[page, direction], setPage] = useState([0, 0]);
+
+    const imageIndex = wrap(0, images.length, page);
+
+    const paginate = (newDirection) => {
+        setPage([page + newDirection, newDirection]);
+    };
 
     const handleNextImage = () => {
-        setCurrentIndex(prevIndex => (prevIndex + 1) % images.length)
+        paginate(1);
     }
 
     const handlePreviousImage = () => {
-        setCurrentIndex(prevIndex => prevIndex > 0 ? prevIndex - 1 : prevIndex);
-    }
-
-    const handleGoToImage = (index) => {
-        setCurrentIndex(index);
+        paginate(-1);
     }
 
     return (
-        <div className='group w-full m-auto relative flex flex-col items-center
+        <div className='w-full m-auto relative flex flex-col items-center
         justify-center px-4 py-2 mb-3'>
-            <LoadingImage
-                src={urlFor(images[currentIndex]).url()}
-                alt="project-screenshot"
-                width={'100%'}
-                height={'250px'} />
+            <div className='w-full my-2 h-[150px] mobile-lg:h-[160px] md:h-[190px] 2xl:h-[250px] relative'>
+                <AnimatePresence custom={direction}>
+                    <motion.img
+                        className='max-w-full absolute w-full h-full rounded-lg drop-shadow-xl'
+                        key={page}
+                        src={urlFor(images[imageIndex]).url()}
+                        custom={direction}
+                        variants={variants}
+                        initial="enter"
+                        animate="center"
+                        exit="exit"
+                        transition={{
+                            x: { type: "spring", stiffness: 250, damping: 30 },
+                            opacity: { duration: 0.2 }
+                        }}
+                        drag="x"
+                        dragConstraints={{ left: 0, right: 0 }}
+                        dragElastic={1}
+                        onDragEnd={(e, { offset, velocity }) => {
+                            const swipe = swipePower(offset.x, velocity.x);
 
-            <div
-            className='absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2
-            group-hover:bg-black/20 text-white ease-in duration-200 cursor-pointer'
-            onClick={handlePreviousImage}>
-                <BsChevronCompactLeft 
+                            if (swipe < -swipeConfidenceThreshold) {
+                                paginate(1);
+                            } else if (swipe > swipeConfidenceThreshold) {
+                                paginate(-1);
+                            }
+                        }}
+                    />
+                </AnimatePresence>
+            </div>
+
+            <BsChevronCompactLeft
                 size={30}
-                 />
-            </div>
+                onClick={handlePreviousImage}
+                className={`${arrowBaseClasses} left-2`}
+            />
 
-            <div 
-            className='absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2
-            group-hover:bg-black/20 text-white ease-in duration-200 cursor-pointer'
-            onClick={handleNextImage}>
-                <BsChevronCompactRight size={30} />
-            </div>
+            <BsChevronCompactRight
+                size={30}
+                onClick={handleNextImage}
+                className={`${arrowBaseClasses} right-2`}
+            />
 
-            <div className='flex absolute -bottom-4 justify-center'>
-                {Array.from(Array(images.length).keys()).map(imageIndex => (
-                    <RxDotFilled 
-                    key={imageIndex}
-                    size={20} 
-                    className={`cursor-pointer ${currentIndex === imageIndex ? 'opacity-100' : 'opacity-30'}`}
-                    onClick={() => handleGoToImage(imageIndex)} />
-                ))}
-            </div>
         </div>
     )
-}
+};
 
-export default ImageSlider
+export default ImageSlider;
