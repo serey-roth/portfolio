e54419764a4c93bab53d0aa1120ec936f37ff457
Serey Roth <sereyratanakroth@gmail.com>
2022-12-28 18:39:34 -0800

    add hero schema

--- sanity/schemas/index.js
+++ sanity/schemas/index.js
@@ -2,5 +2,6 @@ import project from "./project"
 import tool from "./tool"
 import message from "./message";
 import about from "./about";
+import hero from "./hero";
 
-export const schemaTypes = [project, tool, message, about];
+export const schemaTypes = [project, tool, message, about, hero];
