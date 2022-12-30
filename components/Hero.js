5e8158477f657da53ca00520b0de25ea0375c2b8
Serey Roth <sereyratanakroth@gmail.com>
2022-12-29 19:45:20 -0800

    update photo sizing and add resume link in Hero

--- components/Hero.js
+++ components/Hero.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { 
-    AiFillLinkedin, 
-    AiFillGithub, 
-    AiFillFacebook, 
+import {
+    AiFillLinkedin,
+    AiFillGithub,
+    AiFillFacebook,
 } from 'react-icons/ai';
 import { motion } from 'framer-motion';
 
@@ -33,30 +33,30 @@ const opacityVariants = {
     }
 }
 
-const Hero = React.forwardRef(({ 
-    hero: { description, photo, tags } 
+const Hero = React.forwardRef(({
+    hero: { description, photo, tags, resumeURL }
 }, ref) => {
     return (
         <div
         ref={ref}
         id='home'
         className='section mb-10 min-h-screen items-center justify-center'>
-            <motion.div 
+            <motion.div
             variants={containerVariants}
             initial='hidden'
             animate='show'
             className='flex flex-col items-center justify-center gap-5
-            lg:grid grid-cols-2 lg:place-content-center w-full'>
+            lg:grid grid-cols-2 lg:place-content-center w-full relative'>
                 <div className='flex flex-col items-center justify-center lg:justify-start
                 gap-5'>
-                    <motion.h2 
+                    <motion.h2
                     variants={opacityVariants}
                     className='font-bold text-2xl mobile-lg:text-4xl
                     md:text-5xl 2xl:text-8xl text-center lg:text-left w-full'>
                         Hi! I am
-                        <span 
-                            className='text-4xl md:text-7xl 2xl:text-9xl text-gradient'>
-                            <br className='block'/> Serey Roth
+                        <span
+                        className='text-4xl md:text-6xl lg:text-7xl 2xl:text-9xl text-gradient'>
+                            <br className='hidden lg:block' /> Serey Roth
                         </span>.
                     </motion.h2>
                     <motion.div
@@ -65,16 +65,16 @@ const Hero = React.forwardRef(({
                     lg:justify-start gap-2 2xl:text-3xl'>
                         {tags.map((title, index) => (
                             <p
-                            key={index} 
+                            key={index}
                             className='w-fit text-center capitalize 
                             rounded-l-full rounded-r-full drop-shadow-2xl
-                            bg-teal-500 text-white font-semibold 
+                            bg-teal-500 text-white font-semibold
                             px-3 py-2 2xl:px-5 2xl:py-4'>
                                 {title}
                             </p>
                         ))}
                     </motion.div>
-                    <motion.p 
+                    <motion.p
                     variants={opacityVariants}
                     className='w-full md:max-w-[80%] self-center lg:max-w-[400px] 
                     text-center text-lg lg:text-xl lg:text-left 2xl:text-2xl 
@@ -87,41 +87,62 @@ const Hero = React.forwardRef(({
                     className='flex w-full items-center justify-center 
                     lg:justify-start gap-5'>
                         <Link href={LINKEDIN}>
-                            <AiFillLinkedin 
-                            className='text-indigo-500 w-[25px] h-[25px] md:w-[40px] 
+                            <AiFillLinkedin
+                            className='text-indigo-500 w-[30px] h-[30px] md:w-[40px] 
                             md:h-[40px] 2xl:w-[50px] 2xl:h-[50px] drop-shadow-xl'/>
                         </Link>
                         <Link href={GITHUB}>
-                            <AiFillGithub 
-                            className='text-indigo-500 w-[25px] h-[25px] md:w-[40px] 
+                            <AiFillGithub
+                            className='text-indigo-500 w-[30px] h-[30px] md:w-[40px] 
                             md:h-[40px] 2xl:w-[50px] 2xl:h-[50px] drop-shadow-xl'/>
                         </Link>
                         <Link href={FACEBOOK}>
-                            <AiFillFacebook 
-                            className='text-indigo-500 w-[25px] h-[25px] md:w-[40px] 
+                            <AiFillFacebook
+                            className='text-indigo-500 w-[30px] h-[30px] md:w-[40px] 
                             md:h-[40px] 2xl:w-[50px] 2xl:h-[50px] drop-shadow-xl'/>
                         </Link>
+                        <motion.div
+                        animate={{
+                            // bg-[indigo, amber, teal]-500
+                            backgroundColor: [
+                                'rgb(99 102 241)',
+                                'rgb(245 158 11)', 
+                                'rgb(20 184 166)']
+                        }}
+                        transition={{
+                            duration: 2,
+                            repeat: Infinity,
+                            repeatType: "reverse",
+                        }}
+                        className='w-[100px] lg:w-[150px] 2xl:w-[190px] p-3 2xl:py-5 
+                        font-bold rounded-l-full rounded-r-full shadow-inner
+                        shadow-black text-white text-center text-xl xl:text-2xl'>
+                            <Link href={`${resumeURL}?dl=serey_roth_resume`}>
+                                Resume
+                            </Link>
+                        </motion.div>
                     </motion.div>
                 </div>
 
-                <motion.div 
+                <motion.div
                 variants={opacityVariants}
                 initial='initial'
                 className='relative flex items-center justify-center h-[250px]
                 rounded-full w-[250px] mobile-lg:w-[300px] mobile-lg:h-[300px]
-                md:w-[400px] md:h-[400px] lg:w-[420px] lg:h-[420px]
-                xl:w-[530px] xl:h-[530px] 2xl:w-[900px] 2xl:h-[900px] overflow-hidden
+                md:w-[330px] md:h-[330px] lg:w-[450px] lg:h-[450px]
+                xl:w-[480px] xl:h-[480px] 2xl:w-[800px] 2xl:h-[800px] overflow-hidden
                 bg-gradient-to-br from-indigo-900 dark:from-indigo-500 shadow-inner
                 shadow-black dark:shadow-indigo-600 '>
                     <Image
                     src={urlFor(photo).url()}
-                    alt='headshot' 
+                    alt='headshot'
                     width={400}
                     height={400}
-                    className='absolute bottom-0 xl:scale-[130%] xl:bottom-[3rem]
-                    2xl:scale-[160%] 2xl:bottom-[9rem]'
+                    className='absolute bottom-0 xl:scale-[110%] xl:bottom-2
+                    2xl:scale-[180%] 2xl:bottom-[9rem]'
                     />
                 </motion.div>
+
             </motion.div>
         </div>
     )
