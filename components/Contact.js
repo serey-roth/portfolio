6b992d7d09e108b29e8c5aae8d6c4e7599c8da8f
Serey Roth <sereyratanakroth@gmail.com>
2022-12-29 18:20:37 -0800

    add styles for dark mode and change notification font size

--- components/Contact.js
+++ components/Contact.js
@@ -50,10 +50,11 @@ const Contact = React.forwardRef((props, ref) => {
             toast.promise(messagePromise, {
                 loading: 'Sending message...',
                 error: (error) => error.message,
-                success: 'Message sent! Serey will be in touch with you soon.'
+                success: 'Message sent! Serey will be in touch with you shortly.'
             }, {
                 style: {
-                    maxWidth: '300px',
+                    maxWidth: '400px',
+                    fontSize: '20px',
                 }
             })
         }
@@ -67,7 +68,7 @@ const Contact = React.forwardRef((props, ref) => {
             <h1 className='section-heading'>Let&apos;s Connect!</h1>
             <div className='flex flex-col w-full gap-2 sm:grid grid-cols-2'>
                 <div className='flex w-full gap-1 items-center sm:justify-center
-                rounded-lg bg-gray-200/30 p-2'>
+                rounded-lg bg-gray-200/30 dark:bg-white/90 dark:text-black p-2'>
                     <Image 
                     src='/mail.png'
                     alt='email-icon'
@@ -80,7 +81,7 @@ const Contact = React.forwardRef((props, ref) => {
                     href='mailto:sereyratanakroth@gmail.com'>sereyratanakroth@gmail.com</a>
                 </div>
                 <div className='flex w-full gap-1 items-center rounded-lg
-                sm:justify-center bg-gray-200/30 p-2'>
+                sm:justify-center bg-gray-200/30 dark:bg-white/90 dark:text-black p-2'>
                     <Image 
                     src='/phone.png'
                     alt='phone-icon'
@@ -90,7 +91,7 @@ const Contact = React.forwardRef((props, ref) => {
                     />
                     <a 
                     className='text-sm sm:text-lg lg:text-xl' 
-                    href='tel:+1(425)2198352'>(+1) (425)219-8352</a>
+                    href='tel:+1(425)2198352'>(+1) (425) 219-8352</a>
                 </div>
             </div>
             <form 
@@ -106,7 +107,7 @@ const Contact = React.forwardRef((props, ref) => {
                         onChange={handleChange}
                         className='w-full rounded-lg appearance-none outline-none
                         focus:ring-inset focus:bg-gray-200/50 duration-200
-                        ease-in bg-gray-200/30 px-2 py-3'
+                        ease-in bg-gray-200/30 dark:bg-white/90 dark:text-black px-2 py-3'
                     />
                     {hasError.name && (
                         <MdError className='text-rose-500 w-[20px] h-[20px] absolute
@@ -124,7 +125,7 @@ const Contact = React.forwardRef((props, ref) => {
                         onChange={handleChange}
                         className='w-full rounded-lg appearance-none outline-none
                         focus:ring-inset focus:bg-gray-200/50 duration-200
-                        ease-in bg-gray-200/30 px-2 py-3'
+                        ease-in bg-gray-200/30 dark:bg-white/90 dark:text-black px-2 py-3'
                     />
                     {hasError.email && (
                         <MdError className='text-rose-500 w-[20px] h-[20px] absolute
@@ -143,7 +144,7 @@ const Contact = React.forwardRef((props, ref) => {
                         onChange={handleChange}
                         className='w-full rounded-lg appearance-none outline-none
                         focus:ring-inset focus:bg-gray-200/60 duration-200
-                        ease-in bg-gray-200/30 px-2 py-3'
+                        ease-in bg-gray-200/30 dark:bg-white/90 dark:text-black px-2 py-3'
                     />
                     {hasError.message && (
                         <MdError className='text-rose-500 w-[20px] h-[20px] absolute
